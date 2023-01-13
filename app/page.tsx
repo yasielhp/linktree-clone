e720063adca1ecc49a5a9d9f2be17498e6ef2c06
Yasiel Hernández Portal <yasielhp@gmail.com>
2023-01-13 19:10:52 +0000

    feat: :construction: add edge function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
 import Image from 'next/image'
-import data from '../data.json'
 import { LinkCard, GithubIcon, TwitterIcon } from '../components/index'
+import { get } from '@vercel/edge-config'
 
-export default function Home() {
+export const dynamic = 'force-dynamic',
+	runtime = 'edge'
+interface Data {
+	name: string
+	avatar: string
+	links: Link[]
+	socials: Social[]
+}
+
+interface Link {
+	href: string
+	title: string
+}
+
+interface Social {
+	href: string
+	title: string
+	image?: string
+}
+
+export default async function HomePage() {
+	const data: Data | undefined = await get('linktree')
+
+	if (!data) {
+		return (
+			<div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
+				<h1 className="font-bold mt-4 mb-8 text-xl text-white">
+					No data found. Please check your configuration.
+				</h1>
+			</div>
+		)
+	}
+
 	return (
 		<div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
 			<Image
@@ -13,16 +45,16 @@ export default function Home() {
 				height={96}
 			/>
 			<h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
-			{data.links.map(link => (
+			{data.links.map((link: Link) => (
 				<LinkCard key={link.href} {...link} />
 			))}
 			<div className="flex items-center gap-4 mt-8">
-				{data.socials.map(link => {
-					if (link.href.includes('github')) {
-						return <GithubIcon key={link.href} {...link} />
+				{data.socials.map((social: Social) => {
+					if (social.href.includes('github')) {
+						return <GithubIcon key={social.href} {...social} />
 					}
-					if (link.href.includes('twitter')) {
-						return <TwitterIcon key={link.href} {...link} />
+					if (social.href.includes('twitter')) {
+						return <TwitterIcon key={social.href} {...social} />
 					}
 				})}
 			</div>
